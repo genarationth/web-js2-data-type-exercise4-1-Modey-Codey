1b043bb20bbdcc7e20a4c896febf0821e70abff9
Modey Codey <phawngam@hotmail.com>
2024-05-13 14:28:03 +0700

    ex4.1

---
+++ script.js
@@ -0,0 +1,13 @@
+const products = {
+    productId: 1,
+    productName: "Piano",
+    quantityInStock: 1,
+    unitPrice: 11,
+    productDescription: {
+        productBrand: "yamaha",
+        sizeOrQuantity: 1,
+        tasteOrScent: null,
+        color: "black",
+        additionalDescription: null
+    }
+};
